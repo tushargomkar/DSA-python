1d6456222249eff9df85342ada8ad9defe866615
Tushar Gomkar <tgomkar5@gmail.com>
2024-10-09 21:39:44 +0530

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -1,38 +1,62 @@
-# Pattern 1 
-# A
-# AB
-# ABC
-# ABCD
-# ABCDE
+# Pattenrn 3 
+
+# A B C D E 
+# A B C D 
+# A B C 
+# A B 
+# A 
 
 def pattern(n):
     count=1
     for i in range(n):
-        for j in range(i+1):
-            print(chr(65+j),end="")
+        for j in range(n-i):
+            print(chr(65+j),end=" ")
             count=count+1
         print()
         
         
 pattern(5)
 
-# Pattern 2 
+# Pattern 4 
 
-# 1 
-# 2 3 
-# 4 5 6 
-# 7 8 9 10 
-# 11 12 13 14 15 
+# A 
+# B B 
+# C C C 
+# D D D D 
+# E E E E E 
 
 
 def pattern(n):
     count=1
     for i in range(n):
         for j in range(i+1):
-            print(count,end=" ")
+            print(chr(65+i),end=" ")
             count=count+1
         print()
         
         
 pattern(5)
+
+
+
+Pattern 5 
+
+
+
+def pattern(n):
+    count=1
+    for i in range(n):
+        for j in range(i+1):
+            print(chr(65+i),end=" ")
+            count=count+1
+        print()
+        
+        
+pattern(5)
+
+
+
+
+
+
 
