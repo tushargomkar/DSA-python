b9d7299e72c6e59537526083c227c4dd33607ea1
Tushar Gomkar <tgomkar5@gmail.com>
2024-10-10 21:55:43 +0530

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -38,21 +38,51 @@ def pattern(n):
 pattern(5)
 
 
-
-Pattern 5 
-
-
-
+# Pattern 6 
+#       A 
+#     A B A 
+#   A B C B A 
+# A B C D C B A 
 def pattern(n):
-    count=1
+    
+    for i in range(n):
+        for j in range(n-i-1):
+            print(" ",end=" ")
+            # Left Tringle 
+        for j in range(i):
+            print(chr(65+j),end=" ")
+            # Right triangle 
+        for j in range(i,-1,-1):
+            print(chr(65+j),end=" ")
+        print()
+        
+        
+pattern(4)
+
+
+
+# Pattern 7
+# E
+# DE
+# CDE
+# BCDE
+# ABCDE
+def pattern(n):
+    k=n-1
     for i in range(n):
         for j in range(i+1):
-            print(chr(65+i),end=" ")
-            count=count+1
+            print(chr(65+k+j),end="")
+        k=k-1
         print()
         
         
 pattern(5)
+
+
+
+
+
+
 
 
 
