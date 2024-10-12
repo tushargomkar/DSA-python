6241e339384385ffca8456d3472e02f4d791f9a5
Tushar Gomkar <tgomkar5@gmail.com>
2024-10-12 22:27:15 +0530

    Update Day4.py
    # Pattern 2
    # *        *
    # **      **
    # ***    ***
    # ****  ****
    # **********
    # ****  ****
    # ***    ***
    # **      **
    # *        *

--- Day4.py
+++ Day4.py
@@ -35,3 +35,35 @@ def pattern(n):
         
         
 pattern(5)
+
+
+# Pattern 2
+# *        *
+# **      **
+# ***    ***
+# ****  ****
+# **********
+# ****  ****
+# ***    ***
+# **      **
+# *        *
+
+def pattern(n):
+    for i in range(2*n-1):
+        if i<n:
+            for j in range(i+1):
+                print("*",end="")
+            for j in range((n-i-1)*2):
+                print(" ",end="")
+            for j in range(i+1):
+                print("*",end="")
+        else:
+            for j in range(2*n-i-1):
+                print("*",end="")
+            for j in range((i-n+1)*2):
+                print(" ",end="")
+            for j in range(2*n-i-1):
+                print("*",end="")
+        print()
+        
+pattern(5)
