1df405c9973f6b0b3078ae17018bec074ff36aa3
Tushar Gomkar <tgomkar5@gmail.com>
2024-10-24 21:40:28 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -85,3 +85,22 @@ def pattern(n):
         print()
         
 pattern(5)
+
+
+# Pattern 3 
+# *****
+# *   *
+# *   *
+# *   *
+# *****
+def pattern(n):
+    for i in range(n):
+        for j in range(n):
+            if i==0 or i==n-1 or j==0 or j==n-1:
+               print("*",end="")
+            else:
+               print(" ",end="")
+            
+        print()
+        
+pattern(5)
