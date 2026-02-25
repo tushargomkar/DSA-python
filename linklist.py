cfe2d12d7970f9960db9f72aa786e076a915b6f4
Tushar Gomkar <tgomkar5@gmail.com>
2026-02-25 11:02:51 +0530

    Update linklist.py
    test

--- linklist.py
+++ linklist.py
@@ -1,24 +1,20 @@
-# The manual way of linklist 
-class Node:
-    def __init__(self,value,next=None):
-        self.value = value
-        self.next = next
+# # The manual way of linklist 
+# print(int(-3/2))
+# print(3//1)
+
+# for i in range(1,6):
+#     print(i) 
+
+for i in range(10,1,-1):
+    print(i)   
+
+nums=[1,2,3,4,5]
 
 
+for i,n in enumerate(nums):
+    print(i,n)
 
+nums1=['tuple','list','dict',"a"]
 
-Head=Node(1)
-A=Node(2)
-B=Node(31)
-C=Node(4)
-
-Head.next=A
-A.next=B
-B.next=C
-
-def printF(head):
-    i = head
-    while i is not None:
-        print(i.value)
-        i = i.next
-printF(Head)
+arr = [[i*3+j+1 for j in range(3)] for i in range(3)]
+print(arr)
